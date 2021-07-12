4a35e6d9822c8a8c5d398e0a39edbe5556b41dc0
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-12 13:34:03 +0200

    We are using here SPRING DATA JPA and we created a new interface EmployeeRepository which extends JpaRepository interface.THIS IS DAO LAYER.When we are using SPRING DATA JPA we don't need to make all CRUD(DAO) methods, because this interface provides all CRUD methods automatically.We just need to set Entity type(Employee) and primary key (Integer).

---
+++ src/main/java/com/brane/springboot/crud/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.brane.springboot.crud.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brane.springboot.crud.entity.Employee;
+
+//THIS IS DAO LAYER
+//we using SPRING DATA JPA to extends JpaRepository interface and then we don't need to make all CRUD(DAO) methods.
+//This interface provides all CRUD method automatically.We just need to set Entity type(Employee) and primary key(Integer).
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
