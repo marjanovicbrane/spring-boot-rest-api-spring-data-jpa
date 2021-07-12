c0ab43e3e03c1e54a81e37602b1f0fdc4cd70ab3
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-12 14:12:50 +0200

    For method findById we used Optional class.This is a container object which may or may not contain non-null value.If value is present return true and get returns the value.Otherwise return false.If object contains null, when id is not found in our database, then we can throw for example RuntimeException.

--- src/main/java/com/brane/springboot/crud/service/EmployeeServiceImpl.java
+++ src/main/java/com/brane/springboot/crud/service/EmployeeServiceImpl.java
@@ -1,10 +1,10 @@
 package com.brane.springboot.crud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.brane.springboot.crud.dao.EmployeeRepository;
 import com.brane.springboot.crud.entity.Employee;
@@ -40,7 +40,32 @@ public class EmployeeServiceImpl implements EmployeeService {
 		@Override
 		public Employee findById(int theId) {
 
-			return employeeRepository.findById(theId);
+			//*********************************OPTIONAL************************************
+			//Its findById method retrieves an entity by its id. The return value is Optional<T> . 
+			//Optional<T> is a container object which may or may not contain a non-null value. 
+			//If a value is present, isPresent returns true and get returns the value.Otherwise return false.
+			//When returning an Optional instance, it's a useful hint that there's a possibility 
+			//that the value might not exist.
+			
+			//Optional Class is a container for an object that may contains null.With this Optional class, 
+			//we can semantically told clients that a function they will use may return a null value
+			//that lead into NullPointerException.
+			//******************************************************************************
+			
+
+			Optional<Employee> result = employeeRepository.findById(theId);
+			
+			Employee employee=null;
+			
+			if(result.isPresent()) {
+
+				employee=result.get();
+			}
+			else {
+
+				throw new RuntimeException("There is no employee with the ID - "+theId);
+			}
+			return employee;
 		}
 
 
